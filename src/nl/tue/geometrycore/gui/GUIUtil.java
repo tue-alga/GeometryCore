5da6d93982decaae38fda31dad8559a46c276beb
Meulemans <w.meulemans@tue.nl>
2024-08-20 11:30:12 +0200

    added function to create a standard layout with two draw panels

--- src/nl/tue/geometrycore/gui/GUIUtil.java
+++ src/nl/tue/geometrycore/gui/GUIUtil.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.JSplitPane;
 import nl.tue.geometrycore.geometryrendering.GeometryPanel;
 import nl.tue.geometrycore.gui.sidepanel.TabbedSidePanel;
 
@@ -75,5 +76,52 @@ public class GUIUtil {
      */
     public static JFrame makeMainFrame(String title, GeometryPanel draw, TabbedSidePanel side) {
         return makeMainFrame(title, draw, side, FrameLogo.GEOMETRYCORE);
+    }
+
+    /**
+     * Creates and shows a JFrame window, with a given title. The frame is shown
+     * maximized initially and has minimal dimensions of 300x300. It uses a
+     * border layout to place two draw panel in the center, separated by a
+     * vertical, movable divider, and a tabbed side panel on the left. The
+     * constructed frame terminates the java program on close.
+     *
+     * @param title Title for the JFrame
+     * @param drawLeft Draw panel to be placed left of the divider
+     * @param drawRight Draw panel to be placed right of the divider
+     * @param side Side panel to be placed on the left (may be null)
+     * @return the constructed and shown JFrame
+     */
+    public static JFrame makeMainFrameHorizontalSplit(String title, GeometryPanel drawLeft, GeometryPanel drawRight, TabbedSidePanel side) {
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setLayout(new BorderLayout());
+        frame.setMinimumSize(new Dimension(300, 300));
+        try {
+            List<Image> icons = FrameLogo.GEOMETRYCORE.getLogos();
+            if (icons != null) {
+                frame.setIconImages(icons);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(GUIUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (side != null) {
+            frame.add(side, BorderLayout.WEST);
+        }
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                drawLeft, drawRight);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(500);
+
+        Dimension minimumSize = new Dimension(50, 50);
+        drawLeft.setMinimumSize(minimumSize);
+        drawRight.setMinimumSize(minimumSize);
+
+        frame.add(splitPane, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+        return frame;
     }
 }
