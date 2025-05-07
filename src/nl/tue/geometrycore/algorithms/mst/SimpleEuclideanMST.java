537ad3a3694e2d01720af8227cbc6edd8888379b
Meulemans <w.meulemans@tue.nl>
2025-05-07 13:55:37 +0200

    Simplified implementation for DT and added simple implementation for EMST for point sets

--- src/nl/tue/geometrycore/algorithms/mst/SimpleEuclideanMST.java
+++ src/nl/tue/geometrycore/algorithms/mst/SimpleEuclideanMST.java
@@ -1,12 +1,13 @@
 /*
  * GeometryCore library   
- * Copyright (C) 2019   Wouter Meulemans (w.meulemans@tue.nl)
+ * Copyright (C) 2025   Wouter Meulemans (w.meulemans@tue.nl)
  * 
  * Licensed under GNU GPL v3. See provided license documents (license.txt and gpl-3.0.txt) for more information.
  */
-package nl.tue.geometrycore.algorithms.delaunay;
+package nl.tue.geometrycore.algorithms.mst;
 
 import java.util.Iterator;
+import nl.tue.geometrycore.algorithms.delaunay.DelaunayTriangulation;
 import nl.tue.geometrycore.geometry.Vector;
 import nl.tue.geometrycore.geometry.linear.LineSegment;
 import nl.tue.geometrycore.graphs.simple.SimpleEdge;
@@ -15,38 +16,45 @@ import nl.tue.geometrycore.graphs.simple.SimpleVertex;
 import nl.tue.geometrycore.util.Pair;
 
 /**
- * Convenience class for easily computing the Delaunay triangulation of a set of
- * points.
+ * Convenience class for easily computing the Euclidean minimum spanning tree of
+ * a set of points.
  *
  * @author Wouter Meulemans (w.meulemans@tue.nl)
  * @param <TVector> Actual class of the points being provided
  */
-public class SimpleDelaunayTriangulation<TVector extends Vector> {
+public class SimpleEuclideanMST<TVector extends Vector> {
 
     private final IGraph graph = new IGraph();
 
-    /**
-     * Computes the Delaunay triangulation of the given set of points.
-     *
-     * @param points
-     */
     public void run(Iterable<TVector> points) {
         this.graph.clear();
+
         for (TVector p : points) {
             IVertex v = graph.addVertex(p);
             v.original = p;
         }
+
         DelaunayTriangulation<IGraph, LineSegment, IVertex, IEdge> dt = new DelaunayTriangulation<>(graph, (LineSegment geometry) -> geometry);
         dt.run();
+
+        MinimumSpanningTree<IGraph, LineSegment, IVertex, IEdge> mst = new MinimumSpanningTree<>(graph, (e) -> e.getGeometry().squaredLength());
+        DirectedTreeNode<IGraph, LineSegment, IVertex, IEdge> dtn = mst.computeMinimumSpanningTree(graph.getVertices().get(0));
+
+        for (IEdge edge : dtn.getAllEdgesInSubtree()) {
+            edge.remove = false;
+        }
+
+        int i = 0;
+        while (i < graph.getEdges().size()) {
+            IEdge e = graph.getEdges().get(i);
+            if (e.remove) {
+                graph.removeEdge(e);
+            } else {
+                i++;
+            }
+        }
     }
 
-    /**
-     * The edges of the Delaunay triangulation, given as pairs of objects, being
-     * the original points provided. Only edges from the last call to run() are
-     * provided.
-     *
-     * @return Iterable for the edges in the Delaunay triangulation
-     */
     public Iterable<Pair<TVector, TVector>> edges() {
         Iterator<IEdge> it = graph.getEdges().iterator();
         return () -> {
@@ -90,5 +98,6 @@ public class SimpleDelaunayTriangulation<TVector extends Vector> {
 
     private class IEdge extends SimpleEdge<LineSegment, IVertex, IEdge> {
 
+        private boolean remove = true;
     }
 }
